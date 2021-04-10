cdf1b20327503b88300c387503b458e3e5540720
TsukanovVaycheslav <58068725+TsukanovVaycheslav@users.noreply.github.com>
2021-04-10 09:25:02 +0400

    Add files via upload

---
+++ Quiz Game in WinForm 01/Quiz Game in WinForm 01/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace Quiz_Game_in_WinForm_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuastion = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblQuastion
+            // 
+            this.lblQuastion.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblQuastion.Location = new System.Drawing.Point(12, 274);
+            this.lblQuastion.Name = "lblQuastion";
+            this.lblQuastion.Size = new System.Drawing.Size(776, 49);
+            this.lblQuastion.TabIndex = 1;
+            this.lblQuastion.Text = "Question";
+            this.lblQuastion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 326);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(214, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Tag = "1";
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(529, 344);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(214, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Tag = "2";
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(34, 387);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(214, 37);
+            this.button3.TabIndex = 4;
+            this.button3.Tag = "3";
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(529, 387);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(214, 37);
+            this.button4.TabIndex = 5;
+            this.button4.Tag = "4";
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Quiz_Game_in_WinForm_01.Properties.Resources.titanic;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 236);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblQuastion);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblQuastion;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Quiz Game in WinForm 01/Quiz Game in WinForm 01/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Quiz_Game_in_WinForm_01
+{
+    public partial class Form1 : Form
+    {
+        int correctAnswer;
+        int questionNumber = 1;
+        int score;
+        int percentage;
+        int totalQuestions;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            askQuestion(questionNumber);
+
+            totalQuestions = 4; // Stack вопросов
+        }
+
+        private void checkAnswerEvent(object sender, EventArgs e)
+        {
+            var senderObject = (Button)sender;
+
+            int buttonTag = Convert.ToInt32(senderObject.Tag);
+
+            if (buttonTag == correctAnswer)
+            {
+                score++;
+            }
+
+            if (questionNumber == totalQuestions)
+            {
+                percentage = (int)Math.Round((double)(score * 100) / totalQuestions);
+
+                MessageBox.Show(
+
+                    "Quiz Ended!" + Environment.NewLine +
+                    "You have answered " + score + " question." + Environment.NewLine +
+                    "Your total percetage is " + percentage + "%" + Environment.NewLine +
+                    "Click OK to play again"
+                );
+
+                score = 0;
+                questionNumber = 0;
+                askQuestion(questionNumber);
+
+            }
+
+            questionNumber++;
+            askQuestion(questionNumber);
+
+        }
+
+        private void askQuestion(int qnam)
+        {
+            switch (qnam)
+            {
+                case 1:
+
+                    pictureBox1.Image = Properties.Resources.sky; // Картинка
+
+                    lblQuastion.Text = "What is color of the sky?";    // Вопрос
+
+                    button1.Text = "Black";
+                    button2.Text = "Yellow";
+                    button3.Text = "Blue";
+                    button4.Text = "Red";
+
+                    correctAnswer = 3;
+
+                    break;
+
+                case 2:
+                    pictureBox1.Image = Properties.Resources.titanic; // Картинка
+
+                    lblQuastion.Text = "In what year did the Titanic sink in the Atlantic Ocean on April 15 during its maiden voyage from Southampton??"; // Вопрос
+
+                    button1.Text = "1905 year";
+                    button2.Text = "1900 year";
+                    button3.Text = "1912 year";
+                    button4.Text = "1010 year";
+
+                    correctAnswer = 3;
+
+                    break;
+
+                case 3:
+                    pictureBox1.Image = Properties.Resources.company; // Картинка
+
+                    lblQuastion.Text = "What is the name of the largest technology company in South Korea?"; // Вопрос
+
+                    button1.Text = "LOTTE CHEMICAL";
+                    button2.Text = "Wells Fargo";
+                    button3.Text = "Hyundai Wia";
+                    button4.Text = "Berkshire Hathaway";
+
+                    correctAnswer = 1;
+
+                    break;
+
+
+                case 4:
+                    pictureBox1.Image = Properties.Resources.state; // Картинка
+
+                    lblQuastion.Text = "What is the capital of Portugal?"; // Вопрос
+
+                    button1.Text = "Portuguese Republic";
+                    button2.Text = "Spain";
+                    button3.Text = "Madrid";
+                    button4.Text = "Lisbon";
+
+                    correctAnswer = 4;
+
+                    break;
+            }
+        }
+    }
+}
